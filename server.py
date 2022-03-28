93758dc8e3b96fc6926fa6d525776cbab3b44701
KronosGR <geo.elgeo@gmail.com>
2022-03-28 19:55:25 +0200

    changed some html

--- server.py
+++ server.py
@@ -8,16 +8,6 @@ def my_home():
     return render_template('./index.html')
 
 
-@app.route('/about.html')
-def about():
-    return render_template('./about.html')
-
-
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
-
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
