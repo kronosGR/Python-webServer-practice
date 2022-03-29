fd97fbfc160d7d0ded45e3517c4c1309363cd901
KronosGR <geo.elgeo@gmail.com>
2022-03-29 19:38:49 +0200

    get data from contact form

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -11,3 +11,13 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'wrong'
